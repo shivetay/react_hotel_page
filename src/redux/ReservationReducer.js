f094011e6a33e8876ef3f2502608b21cb93c6e91
Łukasz Dawidowicz <lddev@outlook.com>
2021-04-07 07:50:26 +0200

    add url

--- src/redux/ReservationReducer.js
+++ src/redux/ReservationReducer.js
@@ -29,9 +29,7 @@ export const roomLoading = (payload) => ({ payload, type: ROOM_LOADING });
 export const fetchSearchRequest = (peopleId, bedType) => {
   return async (dispatch) => {
     try {
-      const res = await axios.get(
-        `http://localhost:localhost:4000/rooms?people=${peopleId}`
-      );
+      const res = await axios.get(`http://${url}/rooms?people=${peopleId}`);
       dispatch(searchRooms(res.data));
       dispatch(loadSuccess({ name: 'LOAD_SUCCESS' }));
     } catch (err) {
